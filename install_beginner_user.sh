fc8c90a07c0202d847dc7e4320279613897b9a03
Charlie LACROIX <charlie.lacroix9@gmail.com>
2016-11-26 13:30:10 +0100

    Adding the Monokai Theme to vim folder

--- install_beginner_user.sh
+++ install_beginner_user.sh
@@ -6,9 +6,11 @@ sudo apt-get install git;
 
 #Create useful directory
 mkdir -p ${HOME}/.vim/bundle
+mkdir -p ${HOME}/.vim/colors
 
 #Install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+cp monokai.vim ${HOME}/.vim/colors/.
 cp .vimrc_beginner ${HOME}/.vimrc
 
 #Run the VundleUpdate from commandline
